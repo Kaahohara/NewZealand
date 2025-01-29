49a18e00457c325f6411a0a5a48e88811acf3ec3
Karina <liraoharak@gmail.com>
2025-01-28 23:28:30 -0300

    Fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
- 
+  base: '/NewZealand/', // Corrige o caminho base para GitHub Pages
   build: {
-    outDir: 'dist', // Certifica que os arquivos vão para a pasta dist
+    outDir: 'dist',
     rollupOptions: {
-      input: 'index.html', // Certifica que o caminho do HTML está correto
+      input: 'index.html',
     },
   },
 })
