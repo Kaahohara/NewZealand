7b477dacac7a88be3abb1b9e1aaf6c5a7c822bbf
Karina <liraoharak@gmail.com>
2025-01-28 23:25:06 -0300

    Fix: git pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NewZealand/',
+ 
+  build: {
+    outDir: 'dist', // Certifica que os arquivos vão para a pasta dist
+    rollupOptions: {
+      input: 'index.html', // Certifica que o caminho do HTML está correto
+    },
+  },
 })
